10d12f71517afa0649c24ddf95b826b728322ab4
JackPhong <ngotienphong053@gmail.com>
2023-04-05 19:55:45 +0700

    Video 11-1. pool, promises database

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,24 +1,11 @@
-import connection from '../configs/connectDB';
+import pool from '../configs/connectDB';
 
-const getHomepage = (req, res) => {
-    let data = [];
-    connection.query(
-        'SELECT * FROM `users`',
-        (err, results, fields) => {
-            // console.log(`>>Check mysql`);
-            results.map((row) => {
-                data.push({
-                    id: row.id,
-                    email: row.email,
-                    address: row.address,
-                    firstName: row.firstName,
-                    lastName: row.lastName
-                })
-            })
-            return res.render('./index.ejs', { dataUser: data });
-        }
-    );
+const getHomepage = async (req, res) => {
+
+    const [rows, fields] = await pool.execute('SELECT * FROM `users`');
+    return res.render('./index.ejs', { dataUser: rows });
 }
+
 
 
 module.exports = {
